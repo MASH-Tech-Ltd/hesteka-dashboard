33d00b2447ecca38a455d24fde68e0d2aab16081
Sabbir Hossain <sabbir00921@gmail.com>
2026-09-15 02:42:10 +0600

    feat: create SocketContext for WebSocket connection management

--- src/context/SocketContect.jsx
+++ src/context/SocketContect.jsx
@@ -1,13 +1,12 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_BASE_URL 
-  ? import.meta.env.VITE_API_BASE_URL.replace("/api/v1", "")
-  : "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ["websocket"],
 });
+
 
 export const SocketContext = createContext(socket);
